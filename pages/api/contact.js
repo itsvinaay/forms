dd1ffe10a5f5c3f4f8a69d91e0f8674847a469e7
itsvinaay <99385244+itsvinaay@users.noreply.github.com>
2022-10-13 22:56:43 +0530

    env

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,9 +1,9 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 export default async (req, res) => {
   const { name, email, message, phone } = req.body;
 
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
@@ -13,19 +13,14 @@ export default async (req, res) => {
   });
 
   try {
-    // const emailRes = await transporter.sendMail({
-    //   from: email,
-    //   to: 'adaamr@gmail.com',
-    //   subject: `Contact form submission from ${name}`,
-    //   html: `<p>You have a new contact form submission</p><br>
-    //   <p><strong>Name: </strong> ${name} </p><br>
-    //   <p><strong>Phone: </strong> ${phone} </p><br>
-    //   <p><strong>Message: </strong> ${message} </p><br>
+    const emailRes = await transporter.sendMail({
+      from: req.body.email,
+      to: "776vinay@gmail.com",
+      subject: `Contact form submission from ${req.body.name}`,
+      html: `<p>You have a new contact form submission</p><br><p><strong>Name: </strong> ${name} </p><br><p><strong>Phone: </strong> ${req.body.phone} </p><br><p><strong>Message: </strong> ${req.body.message} </p><br> `,
+    });
 
-    //   `,
-    // });
-
-    console.log('Message Sent');
+    console.log("Message Sent");
   } catch (err) {
     console.log(err);
   }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import { useToast } from '../hooks/useToast';
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useToast } from "../hooks/useToast";
 
 export default function Home() {
   const toast = useToast();
@@ -9,10 +9,10 @@ export default function Home() {
   const router = useRouter();
   async function onSubmitForm(values) {
     let config = {
-      method: 'post',
-      url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
+      method: "post",
+      url: `/api/contact`,
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       data: values,
     };
@@ -23,8 +23,8 @@ export default function Home() {
       if (response.status == 200) {
         reset();
         toast(
-          'success',
-          'Thank you for contacting us, we will be in touch soon.'
+          "success",
+          "Thank you for contacting us, we will be in touch soon."
         );
       }
     } catch (err) {}
@@ -35,7 +35,8 @@ export default function Home() {
       <div className="mx-auto w-full max-w-2xl rounded-xl bg-white p-8 shadow">
         <form
           onSubmit={handleSubmit(onSubmitForm)}
-          className="grid grid-cols-1 gap-y-6">
+          className="grid grid-cols-1 gap-y-6"
+        >
           <div>
             <label for="name" className="sr-only">
               Full name
@@ -46,11 +47,11 @@ export default function Home() {
               ref={register({
                 required: {
                   value: true,
-                  message: 'You must enter your name',
+                  message: "You must enter your name",
                 },
               })}
               className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
-                errors.name ? 'ring-2 ring-red-500' : null
+                errors.name ? "ring-2 ring-red-500" : null
               }`}
               placeholder="Full name"
             />
@@ -68,23 +69,23 @@ export default function Home() {
               ref={register({
                 required: {
                   value: true,
-                  message: 'You must enter your email address',
+                  message: "You must enter your email address",
                 },
                 minLength: {
                   value: 8,
-                  message: 'This is not long enough to be an email',
+                  message: "This is not long enough to be an email",
                 },
                 maxLength: {
                   value: 120,
-                  message: 'This is too long',
+                  message: "This is too long",
                 },
                 pattern: {
                   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: 'This needs to be a valid email address',
+                  message: "This needs to be a valid email address",
                 },
               })}
               className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
-                errors.email ? 'ring-2 ring-red-500' : null
+                errors.email ? "ring-2 ring-red-500" : null
               }`}
               placeholder="Email"
             />
@@ -114,7 +115,7 @@ export default function Home() {
               ref={register({
                 required: {
                   value: true,
-                  message: 'You need to enter your message',
+                  message: "You need to enter your message",
                 },
                 maxLength: {
                   value: 1000,
@@ -122,13 +123,14 @@ export default function Home() {
                 },
                 minLength: {
                   value: 50,
-                  message: 'Your message must be longer than this!',
+                  message: "Your message must be longer than this!",
                 },
               })}
               className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
-                errors.message ? 'ring-2 ring-red-500' : null
+                errors.message ? "ring-2 ring-red-500" : null
               }`}
-              placeholder="Message"></textarea>
+              placeholder="Message"
+            ></textarea>
             <span className="text-red-400 text-sm py-2">
               {errors?.message?.message}
             </span>
@@ -136,7 +138,8 @@ export default function Home() {
           <div>
             <button
               type="submit"
-              className="inline-flex justify-center py-3 px-6 border border-transparent shadow text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              className="inline-flex justify-center py-3 px-6 border border-transparent shadow text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
               Submit
             </button>
           </div>
